ab55bc11ecae63de6b156bd034d35bda100e16ca
Zeuxtronic <zeuxtronic@gmail.com>
2024-10-27 13:03:04 +0200

    Add files via upload

---
+++ CurrentUpdate.lua
@@ -0,0 +1,28 @@
+Update 1.3
+
+[+] Changed Images to be PNGS instead of RBXAssset.
+[+] Added the Startup menu you are on Currently.
+[+] Added Phone scaling to UI Library.
+[+] Recoded the UI Library.
+[+] New animations
+
+[=] Moved github repository
+
+Improved the RGD script heavily:
+    [+] Added "Auto Box"
+    [+] Added "Auto Kill Shopkeepers"
+    [+] Added "Autoclicker"
+	[+] Added "AutoEquipBest"
+    [+] Added "AutoHeal"
+    [+] Added "Enable PotionUI"
+    [+] Added "SwordReach"
+    
+    [+] New Tab: "Render"
+    	[+] Added "Esp Enemies"
+    	[+] Added "Esp Allies"
+    	[+] Added "Esp Players"
+
+[+] Added a new type of supported game: "Engine",
+This type of SG works on engines instead of games
+
+[+] New Engine SG: "Robot64"
