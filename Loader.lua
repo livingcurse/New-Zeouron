971881f8b79d87fa7773a341430e79d157c4f025
Zeuxtronic <zeuxtronic@gmail.com>
2024-11-25 16:44:01 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,7 @@ getgenv().T = loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeuxtro
 
 T.ConstructFolder()
 if not getgenv().ZeouronExecuted or readfile("Zeouron/Settings/Developer.txt") == "true" then
-	T.GetFragment("Startup")(function() 
+    T.GetFragment("Startup")(function() 
     	T.GetFragment("Main")
     end)
 end
