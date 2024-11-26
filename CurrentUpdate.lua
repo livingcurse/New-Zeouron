f18c0a70e1e1be61d1fce7cdc6a5724a33f476c7
Zeuxtronic <zeuxtronic@gmail.com>
2024-11-26 16:51:56 +0200

    Update CurrentUpdate.lua

--- CurrentUpdate.lua
+++ CurrentUpdate.lua
@@ -3,18 +3,18 @@ Update 1.4
 [+] Script Link
 
 (Update 1.31) Startup Menu Improvements:
-	[+] Added a fullscreen button to the Startup changelog
-	[~] Changed Changelog font
-	[+] Changelog textsize resize for phones
+    [+] Added a fullscreen button to the Startup changelog
+    [~] Changed Changelog font
+    [+] Changelog textsize resize for phones
 
 RGD improvements:
-	[~] Anti Lava changes
- 	[+] Auto grab improvements
+    [~] Anti Lava changes
+    [+] Auto grab improvements
 
-	[+] Skip
- 	[+] Auto skip
-  	[+] Make Night
-   	[+] Always Night
+    [+] Skip
+    [+] Auto skip
+    [+] Make Night
+    [+] Always Night
     [+] Always Day
     [+] Sit droids
     [+] Bring droids
@@ -24,13 +24,13 @@ RGD improvements:
     	[~] Changed Anti Features to the new "Antis" Tab
 
 Tlk prison Improvements:
-	[-] Infinite Stamina no longer appears to Delta users (Because it crashed them)
-	[~] Changed BypassWalkspeed to "Set WalkSpeed"
- 	[~] Set WalkSpeed (BypassWalkspeed) uses Character.Humanoid.WalkSpeed now
-  	[+] Loop Essentinal Tools
-   	[+] Bug Fixes
+    [-] Infinite Stamina no longer appears to Delta users (Because it crashed them)
+    [~] Changed BypassWalkspeed to "Set WalkSpeed"
+    [~] Set WalkSpeed (BypassWalkspeed) uses Character.Humanoid.WalkSpeed now
+    [+] Loop Essentinal Tools
+    [+] Bug Fixes
 
 UI library Improvements:
-	[+] Configs: Features will now save when executing a SA
- 	[+] Visual Improvements
-  	[+] General improvements
+    [+] Configs: Features will now save when executing a SA
+    [+] Visual Improvements
+    [+] General improvements
