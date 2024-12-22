9de90b25f914223b76e89cc48b33690c24df3ddb
Zeuxtronic <zeuxtronic@gmail.com>
2024-12-22 19:05:51 +0200

    Add files via upload

--- Fragments/DataEditor.lua
+++ Fragments/DataEditor.lua
@@ -12,6 +12,23 @@ for i,v in pairs(game.CoreGui:GetChildren()) do
 	end
 end
 
+downsize = function(descendant)
+    if T.IsPhone() then
+    descendant.Size = UDim2.new(
+            		descendant.Size.X.Scal,
+        	    	descendant.Size.X.Offset / 1.5, 
+        	    	descendant.Size.Y.Scale, 
+      	     		descendant.Size.Y.Offset / 1.5
+       			)
+     			descendant.Position = UDim2.new(
+            		descendant.Position.X.Scale, 
+            		descendant.Position.X.Offset / 1.5, 
+            		descendant.Position.Y.Scale, 
+            		descendant.Position.Y.Offset / 1.5
+        		)
+    end
+end
+
 local G = Instance.new("ScreenGui", game.CoreGui)
 G.ResetOnSpawn = false
 G.Name = "TableEditor"
@@ -29,6 +46,8 @@ MainFrame.ScrollBarImageTransparency = 0
 MainFrame.CanvasSize = UDim2.new(0,0,0,0)
 MainFrame.AnchorPoint = Vector2.new(0.5,0.5)
 
+downsize(MainFrame)
+
 local TitleLabel = Instance.new("TextButton", MainFrame)
 
 TitleLabel.Size = UDim2.new(1,0,0,30)
@@ -38,6 +57,8 @@ TitleLabel.Text = "Data-editor"
 TitleLabel.Font = Data.Font
 TitleLabel.TextScaled = true
 TitleLabel.TextColor3 = Data.Color
+
+downsize(TitleLabel)
 
 local X = Instance.new("TextButton", MainFrame)
 
@@ -49,6 +70,8 @@ X.Font = Data.Font
 X.TextScaled = true
 X.TextColor3 = Data.Color
 X.ZIndex = 2
+
+downsize(X)
 
 X.MouseButton1Click:Connect(function()
     G.Enabled = false
@@ -68,6 +91,8 @@ SearchBox.Font = Data.Font
 SearchBox.TextScaled = true
 SearchBox.TextColor3 = Data.Color
 
+downsize(SearchBox)
+
 local ValueFrame = Instance.new("ScrollingFrame", MainFrame)
 
 ValueFrame.Size = UDim2.new(1,-20,1,-147)
@@ -78,6 +103,8 @@ ValueFrame.ScrollBarImageColor3 = Data.Color
 ValueFrame.ScrollBarImageTransparency = 1
 ValueFrame.ElasticBehavior = "Never"
 ValueFrame.ScrollingDirection = "Y"
+
+downsize(ValueFrame)
 
 charscriptvals = {}
 for i,v in pairs(getsenv(game.Players.LocalPlayer.PlayerScripts.CharacterScript)) do
@@ -117,22 +144,37 @@ update = function(t)
 	 	ValueBox.BackgroundTransparency = 1
 		ValueBox.Text = i.." = "..tostring(v)
 		ValueBox.Font = Data.Font
-		ValueBox.TextSize = 30
+  		if not T.IsPhone then
+			ValueBox.TextSize = 30
+  		else
+    		ValueBox.TextSize = 30 /1.5
+     	end
 		ValueBox.TextColor3 = Data.Color
   		ValueBox.ClearTextOnFocus = false
   		ValueBox.TextXAlignment = "Left"
-    	ValueFrame.CanvasSize = UDim2.new(0,0,0,line *25 -25)
+    	if not T.IsPhone then
+    		ValueFrame.CanvasSize = UDim2.new(0,0,0,line *25 -25)
+     	else
+      		ValueFrame.CanvasSize = UDim2.new(0,0,0,line *16.66 -16.66)
+     	end
     	ValueBox.Name = i
+     
+     	downsize(ValueBox)
      
      	local CopyButton = Instance.new("TextButton", ValueFrame)
 
 		CopyButton.Size = UDim2.new(0,20,0,20)
-		CopyButton.Position = UDim2.new(1,-42.5,0,line *25 -25 +2.5)	
 	 	CopyButton.BackgroundColor3 = Data.DarkC
    		CopyButton.Text = "C"
 		CopyButton.Font = Data.Font
 		CopyButton.TextScaled = true
 		CopyButton.TextColor3 = Data.Color
+  		downsize(ValueBox)
+    	if not T.IsPhone then
+			CopyButton.Position = UDim2.new(1,-42.5,0,line *25 -25 +2.5)	
+  		else
+    		CopyButton.Position = UDim2.new(1,-28.33,0,line *16.66 -16.66 +1.66)
+    	end
        
        	CopyButton.MouseButton1Click:Connect(function()
             setclipboard(tostring("getsenv(game.Players.LocalPlayer.PlayerScripts.CharacterScript)."..i.." = "..tostring(v)))
@@ -181,6 +223,8 @@ BarFrame.Size = UDim2.new(1,-40,0,35)
 BarFrame.Position = UDim2.new(0,20,0,40)
 BarFrame.BackgroundTransparency = 1
 
+downsize(BarFrame)
+
 local BarButtons = 0
 NewBar = function(name,wb,setlist)
     local BarButton = Instance.new("TextButton", BarFrame)
@@ -194,6 +238,8 @@ NewBar = function(name,wb,setlist)
 	BarButton.Font = Data.Font
 	BarButton.TextScaled = true
 	BarButton.TextColor3 = Data.Color
+ 
+ 	downsize(BarButton)
  
  	BarButton.MouseButton1Click:Connect(function()
       	update(getsenv(game.Players.LocalPlayer.PlayerScripts.CharacterScript))
@@ -219,3 +265,4 @@ NewBar("Important",true,{
 NewBar("Fangame",false,{typing = 0, hasfly = 0, dancerp = 0, snapshotcam = 0, polerp = 0, ms = 0, onejump = 0, roperp = 0, respawn = 0, gotice = 0, oldoldformatlevel = 0, st1 = 0, trulyconsole = 0, hasboard = 0, dir = 0, torope = 0, decodelevel = 0, particle = 0, ignore = 0, textbuttons = 0, canmove = 0, cast = 0, playsort = 0, camcf = 0, gtick = 0, mslock = 0, bdamp = 0, trsdeb = 0, rotand = 0, storeclick = 0, plam = 0, maxeggs = 0, bgm = 0, faceid = 0, flying = 0, formatlevel = 0, lvlpackssort = 0, tticket = 0, studder = 0, swip = 0, savetick = 0, anim = 0, playing = 0, bforce = 0, fallvr = 0, shared = 0, jump = 0, st2 = 0, starred = 0, damage = 0, ropeh = 0, watertick = 0, toskin = 0, trdo = 0, tojump = 0, scandy = 0, savegame = 0, pause = 0, iceguide = 0, a = 0, swiperp = 0, icedcream = 0, rg3 = 0, candab = 0, loadvis = 0, loadlevel = 0, enemies = 0, yim = 0, totext = 0, hatpage = 0, spinCF = 0, croucherp = 0, zoom = 0, cvu = 0, sticko = 0, fcrouch = 0, base64encode = 0, pound = 0, MouseMove = 0, cfro = 0, bees = 0, camtand = 0, base64decode = 0, walklerp = 0, rolling = 0, lockhats = 0, UI = 0, icebg = 0, djump = 0, wallerp = 0, lookat = 0, snapcam = 0, timeland = 0, todive = 0, scrollex = 0, tostr = 0, anidamp = 0, rollerp = 0, songplay = 0, safetick = 0, dUP = 0, toattack = 0, longerp = 0, snapto = 0, vis = 0, eggs = 0, createguide = 0, maj = 0, script = 0, lvlpacks = 0, spawnice = 0, studio = 0, flypitch = 0, todance = 0, psound = 0, mobile = 0, health = 0, fromlerp = 0, dive = 0, rendist = 0, store = 0, pst = 0, dmgtick = 0, skate = 0, icrt = 0, lp = 0, z1 = 0, stillcam = 0, nightmode = 0, speedy = 0, transition = 0, icedfound2 = 0, icedfound = 0, button = 0, attacktick = 0, lockcam = 0, ag = 0, ms2 = 0, indoors = 0, s = 0, wallnorm = 0, boarderp = 0, w = 0, d = 0, char = 0, loadmap = 0, crouchtick = 0, dLF = 0, oldolddecodelevel = 0, slowed = 0, trsing = 0, fromani = 0, flamecf = 0, dancing = 0, map = 0, safecf = 0, icr = 0, _G = 0, talktn = 0, btntick = 0, scale = 0, flameon = 0, swimerp = 0, potand = 0, ropetick = 0, hitenemy = 0, mapswitching = 0, botand = 0, sliding = 0, holderp = 0, console = 0, hasflame = 0, shrink = 0, trsCF = 0, dRT = 0, blink = 0, min = 0, skaterp = 0, mobst = 0, out = 0, prog = 0, frame = 0, rf = 0, aterp = 0, oldformatlevel = 0, olddecodelevel = 0, debounce = 0, fastmode = 0, fallerp = 0, walkfx = 0, skatestat = 0, sliderp = 0, ledgepoint = 0, tokens = 0, skin = 0, touched = 0, make = 0, anim2 = 0, pzom = 0, lederp = 0, hat = 0, lockskin = 0, speed = 0, skipped = 0, candy = 0, particles = 0, selt = 0, campitch = 0, passes = 0, showbis = 0, wt = 0, timer = 0, givedata = 0, swimming = 0, attack = 0, tocrouch = 0, dt = 0, point = 0, vcf = 0, carry = 0, uis = 0, z2 = 0, walktick = 0, MouseBehavior = 0, hpdeb = 0, limit = 0, jumping = 0, tpc = 0, v2 = 0, dDN = 0, bossfight = 0, dab = 0, flipo = 0, cvt = 0, crouch = 0, flamerp = 0, lookerp = 0, gd = 0, smoothing = 0, firstperson = 0, longjump = 0, textbox = 0, flyspeed = 0, tscale = 0, earlystart = 0, mps = 0, deadzone = 0, ledgegrab = 0,infwater = 0,paused = 0,wallrun = 0})
 
 return {G,update}
+
