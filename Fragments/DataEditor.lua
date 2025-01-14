a764417936bfee7ccf72a4f9361ca89b2c135b06
Zeuxtronic <zeuxtronic@gmail.com>
2025-01-14 21:31:09 +0200

    Add files via upload

--- Fragments/DataEditor.lua
+++ Fragments/DataEditor.lua
@@ -1,8 +1,6 @@
 lp = game.Players.LocalPlayer
 UIS = game:GetService("UserInputService")
 
-print("vore")
-
 local Icons = T.GetLibrary("Icons")
 
 local WhiteL = false
@@ -15,7 +13,6 @@ for i,v in pairs(game.CoreGui:GetChildren()) do
 end
 
 local IsPhone = T.IsPhone
-
 downsize = function(descendant)
     if IsPhone() then
     	descendant.Size = UDim2.new(
@@ -30,6 +27,9 @@ downsize = function(descendant)
             descendant.Position.Y.Scale, 
             descendant.Position.Y.Offset / 1.5
         )
+        if descendant:IsA("TextButton") or descendant:IsA("TextLabel") then
+            descendant.TextSize = descendant.TextSize /1.5
+        end
     end
 end
 
@@ -75,11 +75,11 @@ X.TextScaled = true
 X.TextColor3 = Data.Color
 X.ZIndex = 2
 
-downsize(X)
-
 X.MouseButton1Click:Connect(function()
     G.Enabled = false
 end)
+
+downsize(X)
 
 local SearchBox = Instance.new("TextBox", MainFrame)
 
@@ -115,6 +115,18 @@ for i,v in pairs(getsenv(game.Players.LocalPlayer.PlayerScripts.CharacterScript)
     charscriptvals[i] = 0
 end
 
+round = function(num)
+    return math.round(num *100) /100
+end
+
+local filters = {
+    ["boolean"] = true,
+    ["string"] = true,
+    ["number"] = true,
+    ["Instance"] = false,
+    ["function"] = false
+}
+
 update = function(t)
     if not UIS:GetFocusedTextBox() or UIS:GetFocusedTextBox().Parent ~= ValueFrame then
     t = t
@@ -137,16 +149,41 @@ update = function(t)
             	ignore = false
             end
     	end
+ 		if filters[typeof(v)] ~= true then
+       		ignore = true
+      	end
      
-     	if typeof(v) == "number" and charscriptvals[i] == 0 and i:match(SearchBox.Text) and not ignore or typeof(v) == "boolean" and charscriptvals[i] == 0 and i:match(SearchBox.Text) and not ignore then
+    	local searchstring = i..tostring(v)
+     	if 
+      		typeof(v) == "number" and charscriptvals[i] == 0 and searchstring:match(SearchBox.Text) and not ignore or 
+      		typeof(v) == "boolean" and charscriptvals[i] == 0 and searchstring:match(SearchBox.Text) and not ignore or
+        	typeof(v) == "string" and charscriptvals[i] == 0 and searchstring:match(SearchBox.Text) and not ignore or
+         	typeof(v) == "Instance" and charscriptvals[i] == 0 and searchstring:match(SearchBox.Text) and not ignore or
+          	typeof(v) == "function" and charscriptvals[i] == 0 and searchstring:match(SearchBox.Text) and not ignore 
+      	then
  		line += 1
    
-   		local ValueBox = Instance.new("TextBox", ValueFrame)
+   		local ValueBox
+   		if typeof(v) ~= "Instance" or typeof(v) ~= "function" then
+   			ValueBox = Instance.new("TextBox", ValueFrame)
+      	else
+       		ValueBox = Instance.new("TextLabel", ValueFrame)
+       	end
 
 		ValueBox.Size = UDim2.new(1,0,0,25)
 		ValueBox.Position = UDim2.new(0,0,0,line *25 -25)	
 	 	ValueBox.BackgroundTransparency = 1
-		ValueBox.Text = i.." = "..tostring(v)
+   		if typeof(v) == "string" then
+   			ValueBox.Text = i.." = \""..tostring(v).."\""
+   		elseif typeof(v) == "number" then
+   			ValueBox.Text = i.." = "..tostring(round(v))
+   		elseif typeof(v) == "Instance" then
+     		ValueBox.Text = i.." = "..v.Name
+     	elseif typeof(v) == "function" then
+      		ValueBox.Text = "function "..i.."()"
+      	else
+     		ValueBox.Text = i.." = "..tostring(v)
+      	end
 		ValueBox.Font = Data.Font
   		if not IsPhone() then
 			ValueBox.TextSize = 30
@@ -154,7 +191,9 @@ update = function(t)
     		ValueBox.TextSize = 30 /1.5
      	end
 		ValueBox.TextColor3 = Data.Color
-  		ValueBox.ClearTextOnFocus = false
+  		if typeof(v) ~= "Instance" then
+  			ValueBox.ClearTextOnFocus = false
+    	end
   		ValueBox.TextXAlignment = "Left"
     	if not IsPhone() then
     		ValueFrame.CanvasSize = UDim2.new(0,0,0,line *25 -25)
@@ -164,6 +203,27 @@ update = function(t)
     	ValueBox.Name = i
      
      	downsize(ValueBox)
+      
+      	if typeof(v) == "function" then
+        local RunButton = Instance.new("TextButton", ValueFrame)
+
+		RunButton.Size = UDim2.new(0,20,0,20)
+	 	RunButton.BackgroundColor3 = Data.DarkC
+   		RunButton.Text = "R"
+		RunButton.Font = Data.Font
+		RunButton.TextScaled = true
+		RunButton.TextColor3 = Data.Color
+  		downsize(RunButton)
+  		if not IsPhone() then
+  			RunButton.Position = UDim2.new(1,-67.5,0,line *25 -25 +2.5)	
+     	else
+      		RunButton.Position = UDim2.new(1,-67.5 /1.5,0,line *(25 /1.5) -(25 /1.5) +(2.5 /1.5))	
+      	end
+   
+   		RunButton.MouseButton1Click:Connect(function()
+        	t[i]()
+        end)
+        end
      
      	local CopyButton = Instance.new("TextButton", ValueFrame)
 
@@ -173,16 +233,34 @@ update = function(t)
 		CopyButton.Font = Data.Font
 		CopyButton.TextScaled = true
 		CopyButton.TextColor3 = Data.Color
-  		CopyButton.Position = UDim2.new(1,-42.5,0,line *25 -25 +2.5)	
   		downsize(CopyButton)
+  		if not IsPhone() then
+  			CopyButton.Position = UDim2.new(1,-42.5,0,line *25 -25 +2.5)	
+     	else
+      		CopyButton.Position = UDim2.new(1,-42.5 /1.5,0,line *(25 /1.5) -(25 /1.5) +(2.5 /1.5))	
+      	end
        
        	CopyButton.MouseButton1Click:Connect(function()
-            setclipboard(tostring("getsenv(game.Players.LocalPlayer.PlayerScripts.CharacterScript)."..i.." = "..tostring(v)))
+            if typeof(v) == "number" or typeof(v) == "boolean" then
+            	setclipboard(tostring("getsenv(game.Players.LocalPlayer.PlayerScripts.CharacterScript)."..i.." = "..tostring(v)))
+            elseif typeof(v) == "string" then
+            	setclipboard(tostring("getsenv(game.Players.LocalPlayer.PlayerScripts.CharacterScript)."..i.." = \""..v.."\""))
+            elseif typeof(v) == "Instance" then
+        		setclipboard(tostring("getsenv(game.Players.LocalPlayer.PlayerScripts.CharacterScript)."..i.." = game."..v:GetFullName()))
+          	elseif typeof(v) == "function" then
+           		setclipboard(tostring("getsenv(game.Players.LocalPlayer.PlayerScripts.CharacterScript)."..i.."()"))
+         	end
         end)
+    	if typeof(v) ~= "Instance" or typeof(v) ~= "function" then
     	ValueBox.Changed:Connect(function() 
          	local MaxLength = #string.split(i,"") + 3
           	if #string.split(ValueBox.Text,"") <MaxLength then
         		ValueBox.Text = i.." = "
+         	end
+        end)
+    	ValueBox.Focused:Connect(function()
+         	if typeof(v) == "string" then
+        		ValueBox.Text = i.." = "..v
          	end
         end)
     	ValueBox.FocusLost:Connect(function() 
@@ -200,6 +278,10 @@ update = function(t)
                		ValueBox.Text = i.." = ".."false"
                	end
          	end
+      		if typeof(v) == "string" then
+            	t[i] = string.split(ValueBox.Text," = ")[2]
+            	ValueBox.Text = i.." = \""..string.split(ValueBox.Text," = ")[2].."\""
+            end
         	if typeof(v) == "number" then
         		t[i] = tonumber(string.split(ValueBox.Text," = ")[2])
           	elseif typeof(v) == "boolean" then
@@ -210,12 +292,110 @@ update = function(t)
                 end
             end
         end)
+    	end
  		end
  	end
 	end
 end
 
 update(getsenv(game.Players.LocalPlayer.PlayerScripts.CharacterScript))
+
+local List = Instance.new("ImageButton", MainFrame)
+
+List.Size = UDim2.new(0,30,0,30)
+List.Position = UDim2.new(0,0,0,0)
+List.BackgroundTransparency = 1
+List.Image = "http://www.roblox.com/asset/?id=8733216068"
+List.ImageColor3 = Data.Color
+List.ZIndex = 2
+
+downsize(List)
+
+local ListFrame = Instance.new("ScrollingFrame", List)
+
+ListFrame.Size = UDim2.new(0,120,0,30)
+ListFrame.BackgroundColor3 = Data.DarkC
+ListFrame.BorderColor3 = Data.Color
+ListFrame.BorderSizePixel = 2
+ListFrame.ScrollBarImageTransparency = 0
+ListFrame.CanvasSize = UDim2.new(0,0,0,0)
+ListFrame.Visible = false
+ListFrame.ZIndex = 10
+downsize(ListFrame)
+if not IsPhone() then
+	ListFrame.Position = UDim2.new(0,40,0,4)
+else
+	ListFrame.Position = UDim2.new(0,(30 /1.5) + 60,0,4)
+end
+
+ListFrameOn = false
+List.MouseButton1Click:Connect(function()
+    if ListFrameOn then
+        ListFrameOn = false
+   	else
+    	ListFrameOn = true
+    end
+	ListFrame.Visible = ListFrameOn
+end)
+
+local index = 0
+for i,f in pairs(filters) do
+    index = index +1
+    local filter = Instance.new("Frame", ListFrame)
+
+	filter.Size = UDim2.new(1,0,0,30)
+	filter.Position = UDim2.new(0,0,0,30 *index -30)
+	filter.BackgroundTransparency = 1
+	filter.ZIndex = 11
+ 
+ 	downsize(filter)
+ 
+ 	local filterText = Instance.new("TextLabel", filter)
+
+	filterText.Size = UDim2.new(1,-35,0,30)
+	filterText.Position = UDim2.new(0,35,0,0)
+	filterText.BackgroundTransparency = 1
+ 	filterText.Text = i
+	filterText.Font = Data.Font
+	filterText.TextScaled = true
+	filterText.TextColor3 = Data.Color
+	filterText.ZIndex = 12
+ 
+ 	downsize(filterText)
+ 
+ 	local Switch = Instance.new("TextButton",filter)
+    Switch.Size = UDim2.new(0,20,0,20)
+   	Switch.Position = UDim2.new(0,5,0,5)
+    Switch.BackgroundColor3 = Data.DarkerC
+    Switch.BorderSizePixel = 0
+    Switch.ZIndex = 12
+    Switch.Text = ""
+    Switch.AutoButtonColor = false
+    
+    downsize(Switch)
+    
+    local onoff = false
+    Switch.MouseButton1Click:Connect(function()
+        if onoff then
+            onoff = false
+           	filters[i] = false
+            Switch.BackgroundColor3 = Data.DarkerC
+        else
+        	onoff = true
+         	filters[i] = true
+         	Switch.BackgroundColor3 = Data.Color
+        end
+    	update(getsenv(game.Players.LocalPlayer.PlayerScripts.CharacterScript))
+    end)
+    
+    if filters[i] == true then
+        onoff = true
+    	Switch.BackgroundColor3 = Data.Color
+    end
+ 
+    ListFrame.Size = UDim2.new(0,120,0,30 *index)
+    downsize(ListFrame)
+end
 
 local BarFrame = Instance.new("Frame", MainFrame)
 
@@ -242,10 +422,9 @@ NewBar = function(name,wb,setlist)
  	downsize(BarButton)
  
  	BarButton.MouseButton1Click:Connect(function()
-      	print("fuck")
-      	update(getsenv(game.Players.LocalPlayer.PlayerScripts.CharacterScript))
     	WhiteL = wb
 		list = setlist
+  		update(getsenv(game.Players.LocalPlayer.PlayerScripts.CharacterScript))
    	end)
  
  	BarButtons += 1
@@ -261,7 +440,10 @@ NewBar("Important",true,{
     infwater = 0,
     hasflame = 0,
     hasfly = 0,
-    hasboard = 0
+    hasboard = 0,
+    damage = 0,
+    savegame = 0,
+    songplay = 0
 })
 NewBar("Fangame",false,{typing = 0, hasfly = 0, dancerp = 0, snapshotcam = 0, polerp = 0, ms = 0, onejump = 0, roperp = 0, respawn = 0, gotice = 0, oldoldformatlevel = 0, st1 = 0, trulyconsole = 0, hasboard = 0, dir = 0, torope = 0, decodelevel = 0, particle = 0, ignore = 0, textbuttons = 0, canmove = 0, cast = 0, playsort = 0, camcf = 0, gtick = 0, mslock = 0, bdamp = 0, trsdeb = 0, rotand = 0, storeclick = 0, plam = 0, maxeggs = 0, bgm = 0, faceid = 0, flying = 0, formatlevel = 0, lvlpackssort = 0, tticket = 0, studder = 0, swip = 0, savetick = 0, anim = 0, playing = 0, bforce = 0, fallvr = 0, shared = 0, jump = 0, st2 = 0, starred = 0, damage = 0, ropeh = 0, watertick = 0, toskin = 0, trdo = 0, tojump = 0, scandy = 0, savegame = 0, pause = 0, iceguide = 0, a = 0, swiperp = 0, icedcream = 0, rg3 = 0, candab = 0, loadvis = 0, loadlevel = 0, enemies = 0, yim = 0, totext = 0, hatpage = 0, spinCF = 0, croucherp = 0, zoom = 0, cvu = 0, sticko = 0, fcrouch = 0, base64encode = 0, pound = 0, MouseMove = 0, cfro = 0, bees = 0, camtand = 0, base64decode = 0, walklerp = 0, rolling = 0, lockhats = 0, UI = 0, icebg = 0, djump = 0, wallerp = 0, lookat = 0, snapcam = 0, timeland = 0, todive = 0, scrollex = 0, tostr = 0, anidamp = 0, rollerp = 0, songplay = 0, safetick = 0, dUP = 0, toattack = 0, longerp = 0, snapto = 0, vis = 0, eggs = 0, createguide = 0, maj = 0, script = 0, lvlpacks = 0, spawnice = 0, studio = 0, flypitch = 0, todance = 0, psound = 0, mobile = 0, health = 0, fromlerp = 0, dive = 0, rendist = 0, store = 0, pst = 0, dmgtick = 0, skate = 0, icrt = 0, lp = 0, z1 = 0, stillcam = 0, nightmode = 0, speedy = 0, transition = 0, icedfound2 = 0, icedfound = 0, button = 0, attacktick = 0, lockcam = 0, ag = 0, ms2 = 0, indoors = 0, s = 0, wallnorm = 0, boarderp = 0, w = 0, d = 0, char = 0, loadmap = 0, crouchtick = 0, dLF = 0, oldolddecodelevel = 0, slowed = 0, trsing = 0, fromani = 0, flamecf = 0, dancing = 0, map = 0, safecf = 0, icr = 0, _G = 0, talktn = 0, btntick = 0, scale = 0, flameon = 0, swimerp = 0, potand = 0, ropetick = 0, hitenemy = 0, mapswitching = 0, botand = 0, sliding = 0, holderp = 0, console = 0, hasflame = 0, shrink = 0, trsCF = 0, dRT = 0, blink = 0, min = 0, skaterp = 0, mobst = 0, out = 0, prog = 0, frame = 0, rf = 0, aterp = 0, oldformatlevel = 0, olddecodelevel = 0, debounce = 0, fastmode = 0, fallerp = 0, walkfx = 0, skatestat = 0, sliderp = 0, ledgepoint = 0, tokens = 0, skin = 0, touched = 0, make = 0, anim2 = 0, pzom = 0, lederp = 0, hat = 0, lockskin = 0, speed = 0, skipped = 0, candy = 0, particles = 0, selt = 0, campitch = 0, passes = 0, showbis = 0, wt = 0, timer = 0, givedata = 0, swimming = 0, attack = 0, tocrouch = 0, dt = 0, point = 0, vcf = 0, carry = 0, uis = 0, z2 = 0, walktick = 0, MouseBehavior = 0, hpdeb = 0, limit = 0, jumping = 0, tpc = 0, v2 = 0, dDN = 0, bossfight = 0, dab = 0, flipo = 0, cvt = 0, crouch = 0, flamerp = 0, lookerp = 0, gd = 0, smoothing = 0, firstperson = 0, longjump = 0, textbox = 0, flyspeed = 0, tscale = 0, earlystart = 0, mps = 0, deadzone = 0, ledgegrab = 0,infwater = 0,paused = 0,wallrun = 0})
 
