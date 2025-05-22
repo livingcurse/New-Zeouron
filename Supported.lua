483070c35b72175d6aa1c1cb31b55fa5b9cf34d1
Zeuxtronic <zeuxtronic@gmail.com>
2025-05-22 18:10:29 +0300

    Add files via upload

--- Supported.lua
+++ Supported.lua
@@ -6,7 +6,7 @@ return {
         GameName = "Tlk Prison",
         WrongSupport = "Please report this to l_l6658",
         Discontinued = true,
-        Script = "Tlk%20prison.lua"
+        Script = "https://raw.githubusercontent.com/Zeuxtronic/New-Zeouron/refs/heads/main/Fragments/Tlk%20prison.lua"
         
     },
 	{
@@ -16,13 +16,22 @@ return {
         GameName = "Randomly Generated Droids",
         WrongSupport = "Please go into a run and execute the script, instead of the Lobby",
         Discontinued = false,
-        Script = "RGD.lua"
+        Script = "https://raw.githubusercontent.com/Zeuxtronic/New-Zeouron/refs/heads/main/Fragments/RGD.lua"
+    },
+	{
+        Type = "GameSupport",
+        ID = 5732973455,
+        GOTOID = 5732973455,
+        GameName = "Hours",
+        WrongSupport = "yahoo Please report this to l_l6658",
+        Discontinued = false,
+        Script = "https://raw.githubusercontent.com/Zeuxtronic/New-Zeouron/refs/heads/main/Fragments/Hours.lua"
     },
 	{
      	Type = "Engine",
         EngineMain = 5561268850,
         EngineName = "Robot 64",
-        Script = "Robot64.lua",
+        Script = "https://raw.githubusercontent.com/Zeuxtronic/New-Zeouron/refs/heads/main/Fragments/Robot64.lua",
     	EngineDetectionMethod = function()
          	local isengine = false
           	if game.Players.LocalPlayer.PlayerScripts:FindFirstChild("CharacterScript") and game:GetService("Workspace"):FindFirstChild("plam") then
