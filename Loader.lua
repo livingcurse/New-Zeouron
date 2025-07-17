416047938ecfa84d72b70b54a2e32381533b7650
livingcurse <120968188+livingcurse@users.noreply.github.com>
2025-07-17 17:03:23 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-getgenv().T = loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeuxtronic/New-Zeouron/refs/heads/main/Library/ZeoTool.lua"))()()
+getgenv().T = loadstring(game:HttpGet("https://raw.githubusercontent.com/livingcurse/New-Zeouron/refs/heads/main/Library/ZeoTool.lua"))()()
 
 T.ConstructFolder()
 if not getgenv().ZeouronExecuted or readfile("Zeouron/Settings/Developer.txt") == "true" then
